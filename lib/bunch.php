d0372c0404e1ded030b5b1ea59577d5338fcc6fa
dennislessa <dennislessa@hotmail.com>
2016-11-22 15:22:27 -0300

    Nova funcionalidade - TGF
    Converte uma DSM para o formato TGF.

--- lib/bunch.php
+++ lib/bunch.php
@@ -7,3 +7,45 @@ function dsm_to_input_bunch(array $dependencias) {
         endforeach;
     endif;
 }
+
+function dsm_to_input_tgf(array $dependencias) {
+    $entidades = [];
+    $aux = [];
+    if (is_array($dependencias)):
+        foreach ($dependencias as $d):
+            $e  = explode(' ', $d);
+            $e1 = $e[0];
+            $e2 = $e[1];
+            
+            if (!in_array($e1, $entidades)):
+                $entidades[] = $e1;
+            endif;
+            
+            if (!in_array($e2, $entidades)):
+                $entidades[] = $e2;
+            endif;
+        endforeach;
+        
+        $aux = $entidades;
+        $entidades = [];
+        for($i=0,$j=count($aux); $i<$j;$i++):
+            $entidades[$aux[$i]] = $i;
+        endfor;
+        
+        foreach($entidades as $i => $v):
+            echo "{$i} {$v}<br/>";
+        endforeach;
+        
+        echo '#<br/>';
+        $i = 0;
+        foreach ($dependencias as $d):
+            $e  = explode(' ', $d);
+            $p  = explode(' ', $aux[$i]);
+            $e1 = $e[0];
+            $e2 = $e[1];
+            
+            echo $entidades[$e1] . ' ' . $entidades[$e2] . '<br/>';
+            $i++;
+        endforeach;
+    endif;
+}
